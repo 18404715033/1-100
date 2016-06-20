789bd160e422c2289815d79882931784017188ab
18404715033 <965630447@qq.com>
2016-06-20 18:31:32 +0800

    Initial Commit

---
+++ 1-100/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  1-100
+//
+//  Created by 李伯阳  Lok’Tar Ogar on 16/6/20.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
